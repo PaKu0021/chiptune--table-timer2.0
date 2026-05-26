d9da7af9adff91dac84cc644ead87f5c980564d6
PaKu0021 <piaojunyi0021@gmail.com>
2026-05-26 15:29:17 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,8 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyDRaQF1N4p9J3q1k0eBNdKzvnwvcAZwY6g",
   authDomain: "chiptune-table-timer.firebaseapp.com",
-  projectId: "chiptune-table-timer"
+  projectId: "chiptune-table-timer",
+  storageBucket: "chiptune-table-timer.firebasestorage.app",
+  messagingSenderId: "524748960844",
+  appId: "1:524748960844:web:958f11454796be7366f0e9",
+  measurementId: "G-68TF8QWDT9"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
