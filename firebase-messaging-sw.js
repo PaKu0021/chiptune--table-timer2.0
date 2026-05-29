341da334659b9a8bf3c95d23d1fe2e4ac4e33da5
PaKu0021 <piaojunyi0021@gmail.com>
2026-05-29 04:11:12 +0000

    Update     firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDRaQF1N4p9J3q1k0eBNdKzvnwvcAZwY6g",
