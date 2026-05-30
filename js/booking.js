a48b244ccbe4c89dbbc49f8178d5e54ca5bc291f
PaKu0021 <piaojunyi0021@gmail.com>
2026-05-30 12:12:46 +0900

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -23,22 +23,23 @@ function render(){
   renderList();
 }
 
-function renderTableOptions (){
-  const box = document.getElementById("tableChecks") ;
+function renderTableOptions(){
+  const box = document.getElementById("tableChecks");
   box.innerHTML = "";
-  box. className ="table-grid";
-  
-  state. tables. forEach( (t,i) =>{
-  const label = document. createElement ("label") ;
-  label. className = "table-item";
-  label. innerHTML =
-    <input type="checkbox" value="${i}">
-    <span class="num" >$｛t.name.replace（"号桌”，""）｝</span>
-    <span class="sub">$*</span>`;
-  `;
-  box.appendChild( label);
-});
+  box.className = "table-grid";
 
+  state.tables.forEach((t,i)=>{
+    const label = document.createElement("label");
+    label.className = "table-item";
+
+    label.innerHTML = `
+      <input type="checkbox" class="table-check" value="${i}">
+      <span class="num">${t.name.replace("号桌","")}</span>
+      <span class="sub">号桌</span>
+    `;
+
+    box.appendChild(label);
+  });
 }
 
 function createBooking(){
